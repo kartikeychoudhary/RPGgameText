20c85809631f2839ec1eabcdec2c9028cf5e8a2e
31shavy@gmail.com <31shavy@gmail.com>
2020-03-23 00:34:48 +0530

    collections added

--- src/script.js
+++ src/script.js
@@ -5,7 +5,36 @@ const BR = "<br>";
 const defaultLine = "+ Start your Journey now !!!"
 
 var environment = new Environment("Forest - ");
+var treasures = new WeakMap();
+var tresCoords = new Set();
 
+var x=0, y=0;
+
+function navigate(stepX, stepY){
+    x+=stepX;
+    y+=stepY;
+
+    let coordiates= document.querySelector("#coordinates");
+    coordiates.innerHTML = `${x}, ${y}`;
+
+    findTreasure();
+}
+
+function findTreasure(){
+    let findCoords;
+    for(let coords of tresCoords){
+        if(coords.x == x && coords.y == y){
+            findCoords = coords;
+        }
+    }
+
+    if(findCoords){
+        var value = treasures.get(findCoords);
+        if(value){
+            addToOutput("You Found " + value.name);
+        }
+    }
+}
 
 function main(){
     let enterEl = document.querySelector("#enter");
@@ -13,6 +42,14 @@ function main(){
     enterEl.addEventListener("click", onclickEnter, false);
 
     addToOutput();
+
+    var coordinate1 = {x:2, y:2};
+    tresCoords.add(coordinate1);
+    treasures.set(coordinate1,{name:"treasure_chest",value:100});
+    var coordinate2 = {x:2, y:0};
+    tresCoords.add(coordinate2);
+    treasures.set(coordinate2,{name:"medaillon",value:10});
+
 }
 main();
 
@@ -28,12 +65,16 @@ function onclickEnter(){
     if(commands.value == "help"){
        options.outputOptions(); 
     }else if(commands.value == "left"){
+        navigate(-1, 0);
         addToOutput(environment.stumbleUpon());
     }else if(commands.value == "right"){
+        navigate(1, 0);
         addToOutput(environment.stumbleUpon());
     }else if(commands.value == "up"){
+        navigate(0, 1);
         addToOutput(environment.stumbleUpon());
     }else if(commands.value == "down"){
+        navigate(0, -1);
         addToOutput(environment.stumbleUpon());
     }
     else{
